34e4e1ffe78d038ae57696363a870a80f8e70f25
Chris Bentivenga <chris.bentivenga@gmail.com>
2011-08-11 09:34:21 -0700

    Regorg in the hopes of doing some more magic with RIT information

--- driver.rb
+++ driver.rb
@@ -1,8 +1,12 @@
-require './DateGrabber'
+require './week/DateGrabber'
 require 'sinatra'
 
 get '/raw' do
 	DateGrabber.new.week.to_s
+end
+
+get '/raw/:d' do |date|
+	DateGrabber.new(Date.parse(date)).week.to_s
 end
 
 get '/' do
